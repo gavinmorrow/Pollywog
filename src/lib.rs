c71c0f45edd7be00b72d19f9d6144af7cdcfd77b
Gavin Morrow <gavinfmorrow@gmail.com>
2023-07-14 16:17:39 -0400

    remove unused code

--- src/lib.rs
+++ src/lib.rs
@@ -10,16 +10,12 @@ pub fn start_app() {
 
     App::new()
         .add_plugins((setup_default_plugins(), PhysicsPlugins::default()))
-        // Physics is run seperatly from the main loop.
-        // It runs on a fixed time step to ensure that the physics is consistent.
-        .insert_resource(FixedTime::new_from_secs(1.0 / 240.0))
         .insert_resource(level::Level::new(0))
         .insert_resource(Gravity(Vec2::new(0.0, -9.81 * 100.0)))
         .add_systems(
             Startup,
             (camera::spawn_camera, player::spawn, level::spawn_blocks),
         )
-        // .add_systems(FixedUpdate, ())
         .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
