6d56ad197a1fed16b0ab94572310090150cbc9d5
Gavin Morrow <gavinfmorrow@gmail.com>
2023-07-14 17:58:03 -0400

    update sample level generation

--- src/level.rs
+++ src/level.rs
@@ -22,11 +22,10 @@ impl Level {
 
         let mut blocks = Vec::with_capacity(num_blocks);
         for i in 0..num_blocks {
-            let translation = Vec3::new(
-                i as f32 * block::SIZE,
-                (i % 5) as f32 * 64.0 + block::SIZE / 2.0,
-                0.0,
-            );
+            let x = i as f32 * block::SIZE;
+            let y = if i % 20 == 0 { block::SIZE } else { 0. } + block::SIZE / 2.0;
+
+            let translation = Vec3::new(x, y, 0.0);
             blocks.push(BlockBundle::new(translation));
         }
 
