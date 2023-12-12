82866756e9f6cf208dda5f073cd346c27b405c19
Gavin Morrow <gavinfmorrow@gmail.com>
2023-12-11 20:08:27 -0500

    rewrite crate::level (including blocks)

--- src/level.rs
+++ src/level.rs
@@ -1,67 +1,27 @@
 use bevy::prelude::*;
 
-use block::{BlockBundle, JumpCollisionBoxBundle};
+use self::block::BlockBundle;
 
-pub mod block;
+mod block;
+
+const SIZE: f32 = 64.0;
 
 #[derive(Default)]
 pub struct LevelPlugin;
 impl Plugin for LevelPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(LevelSprites::new(0))
-            .add_systems(Startup, spawn_blocks);
+        app.add_systems(Startup, spawn_blocks);
     }
 }
 
-struct LevelSprites {
-    // this will be used soon
-    _id: u32,
-    blocks: Vec<BlockBundle>,
-    jump_collision_block_boxes: Vec<JumpCollisionBoxBundle>,
-}
+fn spawn_blocks(mut commands: Commands) {
+    let block = BlockBundle::new(Vec2::new(0.0, 0.0));
+    commands.spawn(block);
 
-impl LevelSprites {
-    fn new(id: u32) -> Self {
-        debug!("Creating level (id: {})", id);
-
-        let num_blocks = match id {
-            0 => 100,
-            id => panic!("Unknown level ID: {}", id),
-        };
-
-        trace!("Creating {} blocks for level", num_blocks);
-
-        let mut blocks = Vec::with_capacity(num_blocks);
-        let mut jump_collision_block_boxes = Vec::with_capacity(num_blocks);
-        for i in 0..num_blocks {
-            let x = i as f32 * block::SIZE;
-            let y = if i % 20 < 15 {
-                0.0
-            } else {
-                (i % 20 - 15) as f32 * block::SIZE
-            };
-
-            let translation = Vec3::new(x, y, 0.0);
-            blocks.push(BlockBundle::new(translation));
-            jump_collision_block_boxes.push(JumpCollisionBoxBundle::new(translation));
-        }
-
-        Self {
-            _id: id,
-            blocks,
-            jump_collision_block_boxes,
-        }
-    }
-}
-
-impl Resource for LevelSprites {}
-
-fn spawn_blocks(mut commands: Commands, level: Res<LevelSprites>) {
-    debug!("Spawning blocks");
-    for block in level.blocks.iter() {
-        commands.spawn(block.clone());
-    }
-    for jump_collision_block_box in level.jump_collision_block_boxes.iter() {
-        commands.spawn(jump_collision_block_box.clone());
+    for i in 0..100 {
+        let x = i as f32 * SIZE;
+        let y = (if i % 8 < 3 { 0 } else { (i - 3) % 5 }) as f32 * SIZE + SIZE;
+        let block = BlockBundle::new(Vec2::new(x, y));
+        commands.spawn(block);
     }
 }
--- src/level/block.rs
+++ src/level/block.rs
@@ -1,10 +1,7 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 
-pub const SIZE: f32 = 64.0;
-pub const SIZE_VEC2: Vec2 = Vec2::new(SIZE, SIZE);
-
-#[derive(Component, Clone, Default)]
+#[derive(Component, Copy, Clone, Default, Debug)]
 pub struct Block;
 
 #[derive(Bundle, Clone)]
@@ -16,31 +13,11 @@ pub struct BlockBundle {
     block: Block,
 }
 
-#[derive(Component, Clone, Default)]
-pub struct JumpCollisionBox;
-
-#[derive(Bundle, Clone)]
-pub struct JumpCollisionBoxBundle {
-    sprite_bundle: SpriteBundle,
-    collider: Collider,
-    jump_collision_box: JumpCollisionBox,
-}
-
-impl core::fmt::Debug for BlockBundle {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.debug_struct(
-            format!(
-                "BlockBundle (transform: {:#?})",
-                self.sprite_bundle.transform
-            )
-            .as_str(),
-        )
-        .finish()
-    }
-}
-
 impl BlockBundle {
-    pub fn new(translation: Vec3) -> Self {
+    pub fn new(translation: Vec2) -> Self {
+        // Needed because `SpriteBundle` requires a `Vec3`
+        let translation = translation.extend(0.0);
+
         trace!("Creating block bundle (translation: {:?})", translation);
 
         Self {
@@ -50,46 +27,19 @@ impl BlockBundle {
                     ..default()
                 },
                 sprite: Sprite {
-                    custom_size: Some(SIZE_VEC2),
+                    custom_size: Some(Vec2::new(64.0, 64.0)),
                     color: Color::rgb(0.22, 0.14, 0.07),
                     ..default()
                 },
                 ..default()
             },
-            // FIXME: Maybe use a kinematic rigid body instead?
-            //
-            // Doing this for now because I'm just trying to get the game
-            // working and we don't have any moving blocks yet
             rigid_body: RigidBody::Fixed,
-            collider: Collider::cuboid(SIZE, SIZE),
-            friction: Friction::new(1.0),
-            block: Block,
-        }
-    }
-}
-
-impl JumpCollisionBoxBundle {
-    pub fn new(translation: Vec3) -> Self {
-        // Add padding on left so wall clinging doesn't work
-        let translation = translation + Vec3::new(1.0, 0.0, 0.0);
-
-        trace!(
-            "Creating jump collision box bundle (translation: {:?})",
-            translation
-        );
-
-        Self {
-            sprite_bundle: SpriteBundle {
-                transform: Transform {
-                    translation,
-                    ..default()
-                },
-                visibility: Visibility::Hidden,
+            collider: Collider::cuboid(32.0, 32.0),
+            friction: Friction {
+                coefficient: 0.5,
                 ..default()
             },
-            // Add padding so wall clinging doesn't work
-            collider: Collider::cuboid(SIZE - 2.0, SIZE - 2.0),
-            jump_collision_box: JumpCollisionBox,
+            block: Block,
         }
     }
 }
