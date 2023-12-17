ba0893f63516e02d74492701a0e85679198088a9
Gavin Morrow <gavinfmorrow@gmail.com>
2023-12-17 08:51:55 -0500

    update camera scrolling (thanks dad!)

--- src/camera.rs
+++ src/camera.rs
@@ -2,8 +2,8 @@ use bevy::{prelude::*, window::PrimaryWindow};
 
 use crate::player::{self, Player};
 
-/// The starting X position of the camera (from the left edge of the window).
-const START_X: f32 = 100.0;
+const MAX_SPEED_X: f32 = 3.0;
+const MAX_SPEED_Y: f32 = 1.0;
 
 #[derive(Default)]
 pub struct CameraPlugin;
@@ -21,8 +21,8 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
     commands.spawn(Camera2dBundle {
         transform: Transform {
             translation: Vec3::new(
-                window.width() / 2.0 - START_X,
-                window.height() / 2.0,
+                window.width() / 4.0,
+                window.height() / 3.0,
                 1000.0,
             ),
             ..default()
@@ -33,32 +33,24 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
 
 pub fn keep_player_in_view(
     mut camera_query: Query<&mut Transform, With<Camera>>,
-    window_query: Query<&Window, With<PrimaryWindow>>,
-    // Using the tuple instead of `With<Player>` allows us to have seperate queries
-    // for the player and the camera (needed b/c they are both accessing `Transform`).
+    // The `Without<Camera>` allows us to have seperate queries for the player
+    // and the camera (needed b/c they are both accessing `Transform`, and if
+    // there somehow was a Transform w/ both `Player` and `Camera` components,
+    // it wouldn't be allowed. we know that it isn't possible, but bevy doesn't)
     player_query: Query<&Transform, (With<Player>, Without<Camera>)>,
 ) {
-    let window = window_query.single();
     let mut camera = camera_query.single_mut();
     let Ok(player) = player_query.get_single() else {
         return;
     };
 
-    let player_x = player.translation.x;
-    let camera_x = camera.translation.x;
+    follow(player.translation.x, &mut camera.translation.x, MAX_SPEED_X);
+    follow(player.translation.y, &mut camera.translation.y, MAX_SPEED_Y);
+}
 
-    const PADDING: f32 = 256.0;
-
-    trace!("player_x: {}, camera_x: {}", player_x, camera_x);
-
-    let distance = camera_x - player_x;
-    if distance.abs() > window.width() / 2.0 - PADDING {
-        debug!("Moving camera to catch up with player");
-
-        let new_x = player_x + (window.width() / 2.0 - PADDING) * distance.signum();
-        trace!("new_x: {}", new_x);
-
-        // move the camera to catch up with the player
-        camera.translation.x = new_x;
-    }
+fn follow(a: f32, b: &mut f32, max_speed: f32) {
+    let delta = *b - a;
+    let delta = delta / 100.0;
+    let delta = if delta.abs() < 0.5 { 0.0 } else { delta };
+    *b -= delta.min(max_speed);
 }
