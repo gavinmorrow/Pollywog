6e6e466f5cfc0bc81582246d2810e6721990d744
gavinmorrow <gavinfmorrow@gmail.com>
2024-02-17 17:49:06 -0500

    add coin bundle

--- src/bundles.rs
+++ src/bundles.rs
@@ -1,2 +1,3 @@
+pub mod coin;
 pub mod enemy;
 pub mod player;
---
+++ src/bundles/coin.rs
@@ -0,0 +1,32 @@
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
+
+const SIZE: f32 = 32.0;
+
+#[derive(Component, Default)]
+pub struct Coin;
+
+#[derive(Bundle)]
+pub struct CoinBundle {
+    collider: Collider,
+    sprite_bundle: SpriteBundle,
+    coin: Coin,
+}
+
+impl CoinBundle {
+    pub fn new(translation: Vec2) -> Self {
+        CoinBundle {
+            collider: Collider::ball(SIZE / 2.0),
+            sprite_bundle: SpriteBundle {
+                sprite: Sprite {
+                    color: Color::YELLOW,
+                    custom_size: Some(Vec2::new(SIZE, SIZE)),
+                    ..default()
+                },
+                transform: Transform::from_translation(translation.extend(0.0)),
+                ..default()
+            },
+            coin: Coin,
+        }
+    }
+}
